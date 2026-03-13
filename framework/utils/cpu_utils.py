ffc5daa8f124b8b9a3ab2771b97862d13b640b10
shymadha <shyamala05@gmail.com>
2026-03-13 10:29:44 +0530

    logger module cleanup

--- framework/utils/cpu_utils.py
+++ framework/utils/cpu_utils.py
@@ -32,28 +32,3 @@ class CpuUtil:
         )
         return module.CpuUtil.get_cpu_info(platform_obj)
 
-# class CpuUtil:
-
-#     @staticmethod
-#     def get_core_count(platform):
-#         os_type = platform.get_os()
-#         if os_type == "linux":
-#             command = "nproc"
-#         elif os_type == "windows":
-#             command = "wmic cpu get NumberOfCores"
-#         else:
-#             raise Exception(f"Unsupported OS: {os_type}")
-#         output, error, status = platform.execute(command)
-#         if status != 0:
-#             raise Exception("Failed to get CPU core count")
-#         return output, error, status
-#         #return CpuUtil._parse_core_count(output, os_type)
-
-
-#     @staticmethod
-#     def _parse_core_count(output, os_type):
-#         if os_type == "linux":
-#             return int(output.strip())
-#         elif os_type == "windows":
-#             lines = output.splitlines()
-#             return int(lines[1].strip())
